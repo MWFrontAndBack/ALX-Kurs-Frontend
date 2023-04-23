60a8711a76dbbc64dafd2d50d482d58e73b4490e
s22288 <forgithubProject1188@gmail.com>
2023-04-23 22:23:48 +0200

    finish train app

--- 08-dom-traintable/js/script.js
+++ 08-dom-traintable/js/script.js
@@ -6,99 +6,103 @@ console.log('trains app')
 // - date (23.04.2023)
 // - name (Sniezka)
 let trains = JSON.parse(localStorage.getItem('trains')) ?? [];
-// [
-//     {
-//         from: "Warszawa",
-//         to: "Lublin",
-//         time: "16:30",
-//         date: new Date('2023-04-23T16:30:00'),
-//         name: "śnieżka"
-//     },
-//     {
-//         from: "Kraków",
-//         to: "Gdańsk",
-//         time: "12:45",
-//         date: new Date('2023-04-24T12:45:00'),
-//         name: "bursztyn"
-//     },
-//     {
-//         from: "Gdynia",
-//         to: "Wrocław",
-//         time: "08:15",
-//         date: new Date('2023-04-25T08:15:00'),
-//         name: "złota jesień"
-//     },
-//     {
-//         from: "Lublin",
-//         to: "Katowice",
-//         time: "20:00",
-//         date: new Date('2023-04-26T20:00:00'),
-//         name: "śnieżka"
-//     },
-//     {
-//         from: "Poznań",
-//         to: "Kraków",
-//         time: "10:30",
-//         date: new Date('2023-04-27T10:30:00'),
-//         name: "bursztyn"
-//     },
-//     {
-//         from: "Gdańsk",
-//         to: "Wrocław",
-//         time: "14:00",
-//         date: new Date('2023-04-28T14:00:00'),
-//         name: "złota jesień"
-//     },
-//     {
-//         from: "Wrocław",
-//         to: "Lublin",
-//         time: "09:20",
-//         date: new Date('2023-04-29T09:20:00'),
-//         name: "śnieżka"
-//     },
-//     {
-//         from: "Lublin",
-//         to: "Gdańsk",
-//         time: "13:45",
-//         date: new Date('2023-04-30T13:45:00'),
-//         name: "złota jesień"
-//     },
-//     {
-//         from: "Kraków",
-//         to: "Warszawa",
-//         time: "18:10",
-//         date: new Date('2023-05-01T18:10:00'),
-//         name: "śnieżka"
-//     },
-//     {
-//         from: "Wrocław",
-//         to: "Kraków",
-//         time: "07:30",
-//         date: new Date('2023-05-02T07:30:00'),
-//         name: "bursztyn"
-//     },
-//     {
-//         from: "Gdańsk",
-//         to: "Poznań",
-//         time: "11:20",
-//         date: new Date('2023-05-03T11:20:00'),
-//         name: "złota jesień"
-//     },
-//     {
-//         from: "Lublin",
-//         to: "Kraków",
-//         time: "22:15",
-//         date: new Date('2023-05-04T22:15:00'),
-//         name: "śnieżka"
-//     },
-//     {
-//         from: "Kraków",
-//         to: "Gdynia",
-//         time: "15:40",
-//         date: new Date('2023-05-05T15:40:00'),
-//         name: "złota jesień"
-//     },
-// ]
+
+let initialization = [
+    {
+        from: "Warszawa",
+        to: "Lublin",
+        time: "16:30",
+        date: new Date('2023-04-23T16:30:00'),
+        name: "śnieżka"
+    },
+    {
+        from: "Kraków",
+        to: "Gdańsk",
+        time: "12:45",
+        date: new Date('2023-04-24T12:45:00'),
+        name: "bursztyn"
+    },
+    {
+        from: "Gdynia",
+        to: "Wrocław",
+        time: "08:15",
+        date: new Date('2023-04-25T08:15:00'),
+        name: "złota jesień"
+    },
+    {
+        from: "Lublin",
+        to: "Katowice",
+        time: "20:00",
+        date: new Date('2023-04-26T20:00:00'),
+        name: "śnieżka"
+    },
+    {
+        from: "Poznań",
+        to: "Kraków",
+        time: "10:30",
+        date: new Date('2023-04-27T10:30:00'),
+        name: "bursztyn"
+    },
+    {
+        from: "Gdańsk",
+        to: "Wrocław",
+        time: "14:00",
+        date: new Date('2023-04-28T14:00:00'),
+        name: "złota jesień"
+    },
+    {
+        from: "Wrocław",
+        to: "Lublin",
+        time: "09:20",
+        date: new Date('2023-04-29T09:20:00'),
+        name: "śnieżka"
+    },
+    {
+        from: "Lublin",
+        to: "Gdańsk",
+        time: "13:45",
+        date: new Date('2023-04-30T13:45:00'),
+        name: "złota jesień"
+    },
+    {
+        from: "Kraków",
+        to: "Warszawa",
+        time: "18:10",
+        date: new Date('2023-05-01T18:10:00'),
+        name: "śnieżka"
+    },
+    {
+        from: "Wrocław",
+        to: "Kraków",
+        time: "07:30",
+        date: new Date('2023-05-02T07:30:00'),
+        name: "bursztyn"
+    },
+    {
+        from: "Gdańsk",
+        to: "Poznań",
+        time: "11:20",
+        date: new Date('2023-05-03T11:20:00'),
+        name: "złota jesień"
+    },
+    {
+        from: "Lublin",
+        to: "Kraków",
+        time: "22:15",
+        date: new Date('2023-05-04T22:15:00'),
+        name: "śnieżka"
+    },
+    {
+        from: "Kraków",
+        to: "Gdynia",
+        time: "15:40",
+        date: new Date('2023-05-05T15:40:00'),
+        name: "złota jesień"
+    },
+]
+
+const jsonList = JSON.stringify(initialization);
+localStorage.setItem("trains", jsonList);
 
 let trainsList = document.querySelector('#trainsList');
 let saerchButton = document.querySelector('#saerchButton')
@@ -106,6 +110,7 @@ let searchInput = document.querySelector('#searchTrainByName')
 let connectionForm = document.querySelector('#addConnection');
 const selectSort = document.querySelector('#trainsSort');
 const selectCity = document.querySelector('#chooseCity')
+const removeButton = document.querySelector('#removeButton')
 function renderCitySelect(collection) {
     selectCity.innerHTML = ""
     collection.forEach((c) => {
@@ -192,9 +197,13 @@ const seaerchCitiesWithConnectionToChoosenCity = (collection) => {
 
 
 }
+const removeAllConections = () => {
+    console.log('delete localstorage')
+    localStorage.clear()
+    render(trains)
+}
 
-
-
+removeButton.addEventListener('click', removeAllConections)
 selectCity.addEventListener('change', () => { seaerchCitiesWithConnectionToChoosenCity(trains) })
 selectSort.addEventListener('change', addSortingOption)
 connectionForm.addEventListener('submit', addNewConnection)
